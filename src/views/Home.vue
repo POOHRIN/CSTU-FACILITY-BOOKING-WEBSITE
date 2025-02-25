6af126ddcf8faf00f2058dae0f001d2e36f8ba15
POOHRIN <147570157+POOHRIN@users.noreply.github.com>
2025-02-25 19:08:08 +0700

    Create login and home page

---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"/>
+
+<template>
+  <main>
+    HOME
+  </main>
+</template>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts"/>
+
+<template>
+    <div>
+        <div>
+            <h2>Login</h2>
+            <form>
+                <div class="mb-4">
+                    <label>Email</label>
+                    <input type="email" required />
+                </div>
+                <div class="mb-4">
+                    <label>Password</label>
+                    <input type="password" required />
+                </div>
+                <RouterLink to="/">Login</RouterLink>
+            </form>
+        </div>
+    </div>
+</template>
+
+<style>
+template{
+    background-color: #c24646;
+}
+div {
+    line-height: 50px;
+    background: #e0e0e0;
+    color: #000;
+}
+input{
+    margin-left: 10px;
+}
+h2{
+    text-align: center;
+}
+</style>

a476d0532cccd4dee93a02290531c58641206600
TINE <tinewisutnata123@gmail.com>
2025-03-12 14:45:27 +0700

    Home Editing

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
+import { useRoute } from "vue-router";
 import Navbar from "../views/Navbar.vue";
 import Sidebar from "../views/Sidebar.vue";
 import Image1 from "@/assets/Test1.jpg";
 import Image2 from "@/assets/Test2.jpg";
+
+const route = useRoute();
 
 </script>
 
@@ -12,6 +15,10 @@ import Image2 from "@/assets/Test2.jpg";
     <div class="main-container">
       <Sidebar />
       <div class="content">
+        <div class="image-container" v-show="route.path === '/home'">
+          <img :src="Image1" alt="Test Image 1" class="image"/>
+          <img :src="Image2" alt="Test Image 2" class="image"/>
+        </div>
         <router-view />
       </div>
     </div>
@@ -48,4 +55,18 @@ import Image2 from "@/assets/Test2.jpg";
   align-items: center;
   padding: 0 20px;
 }
+
+.image-container {
+  display: flex;
+  gap: 20px;
+  margin-bottom: 20px;
+}
+
+.image {
+  width: 200px; 
+  height: auto;
+  border-radius: 10px;
+  box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.2);
+}
+
 </style>
